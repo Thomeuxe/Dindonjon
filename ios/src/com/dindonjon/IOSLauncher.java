a267374a8f56826351d632fb63464443bd4ff2f0
Thomeuxe <thomeuxe@gmail.com>
2015-03-09 10:51:32 +0100

    Setup LibGDX project

---
+++ ios/src/com/dindonjon/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.dindonjon;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.dindonjon.Dindonjon;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new Dindonjon(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
